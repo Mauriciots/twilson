ce3317d09bd37e91277de7d8efa7b5138648a874
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-08-08 16:53:55 -0700

    push test

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-});
+app.get('*', (req, res) => res.sendFile(path.resolve('frontend', 'build', 'index.html'));
 
 let twoots = [];
 
